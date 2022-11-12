401acdd2d92e90d3213bb1ed5f39ee47e900734e
dimka <dimka411841994@gmail.com>
2022-11-12 14:09:05 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+//Дан массив строк, необходимо удалить из него все дубли,
+// оставшиеся строки объединить в одну в порядке следования в массиве.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] mass = {"Первый", "Второй", "Третий", "Четвертый", "Пятый", "Шестой", "Второй", "Четвертый"};
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> list = new ArrayList<>();
+        for (String s : mass) {
+            if (!list.contains(s)) {
+                list.add(s);
+            }
+        }
+        for (String s : list){
+            stringBuilder.append(s);
+        }
+        System.out.println(stringBuilder);
+
+        StringBuilder stringBuilder2 = new StringBuilder();
+        Set<String> setList = new LinkedHashSet<>(Arrays.asList(mass));
+
+        for (String s : setList) {
+            stringBuilder2.append(s);
+        }
+        System.out.println(stringBuilder2);
     }
 }
